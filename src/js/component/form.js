7a090e6182eb575a6bbab065ac5f711b61a4beda
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-05-12 17:23:23 +0000

    missed changes in the previous commit

--- src/js/component/form.js
+++ src/js/component/form.js
@@ -5,9 +5,7 @@ const Form = (props) =>{
     const { list, setList,maped_list, setMap } = props;
     const [item, setItem] = useState ("")
     
-    const handle_change = () =>{
-        console.log("item " + item)
-        // no esta entrando a este ciclo en el primer enter, no se carga list 
+    const handle_change = () =>{        
         const maped_item = list.map( (x) => {            
             return(
                 <li className="list-group-item">
@@ -28,13 +26,12 @@ const Form = (props) =>{
                 <label htmlFor="input" className="visually-hidden">todo</label>
                 <input type="text" onChange={e => setItem(e.target.value)} value={item} onKeyDown={ (e) => {
                                 if (e.key === 'Enter') {
-                                    if(item != ""){
-                                        //no funciona al primer enter
+                                    if(item != ""){                                        
                                         setList(prevList => {
                                             const newList = [...prevList, item];                                        
                                             return newList;
                                         });
-                                        handle_change()                                        
+                                                                               
                                     }
                                         
                                 }
